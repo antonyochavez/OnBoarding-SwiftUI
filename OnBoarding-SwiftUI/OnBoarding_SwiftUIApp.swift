3b178f76244069b0c480009ac77bf099f476adb4
j. Antonio Chavez <jesus.chavez@suramexico.com>
2023-04-05 13:15:08 -0600

    Initial Commit

---
+++ OnBoarding-SwiftUI/OnBoarding_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoarding_SwiftUIApp.swift
+//  OnBoarding-SwiftUI
+//
+//  Created by Antonio Chavez Saucedo on 05/04/23.
+//
+
+import SwiftUI
+
+@main
+struct OnBoarding_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
